9931edd2bdda34ba342e06cf64ac2cb415a7aa22
sriram <sriramgunda@yahoo.com>
2021-04-01 23:47:52 +0530

    find duplicates in list

---
+++ find_duplicates_list/count_duplicates.py
@@ -0,0 +1,13 @@
+from collections import Counter
+  
+# Create a list
+names = ['sriram', 'harish', 'ravi', 'nava', 'harish', 'sriram', 'sriram']
+count_names = Counter(names)
+print(count_names)
+  
+namelist = ['sriram','harish','ravi','nava', 'mouni']
+  
+# Here mouni is not in names 
+# so count of mouni will be zero
+for name in namelist:
+    print (name, count_names[name])
