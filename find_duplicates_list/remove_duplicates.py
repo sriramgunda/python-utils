c1f03e2b780aba6f313521e14bc1f1365a4d23b2
sriram <sriramgunda@yahoo.com>
2021-04-04 23:48:34 +0530

    remove duplicates in list with dict

--- find_duplicates_list/remove_duplicates.py
+++ find_duplicates_list/remove_duplicates.py
@@ -12,14 +12,25 @@ def remove_duplicates_set(lst):
     myset = set(lst)
     list_without_duplicates = list(myset)
     return list_without_duplicates
-    
+
+
+def remove_duplicates_dict(lst):
+    #using dictionaries to identify and gettings unique items in list is also more time saving
+    mydict = list(dict.fromkeys(lst))
+    return mydict
 
 
 if __name__ == '__main__':
     st = "hello"
     fruits = ['apple', 'banana', 'coconut', 'apple', 'grapes', 'apple', 'orange', 'coconut']
+    print("With for loop along with additional list:")
     print(remove_duplicates(st))
     print(remove_duplicates(fruits))
 
+    print("With sets:")
     print(remove_duplicates_set(st))
     print(remove_duplicates_set(fruits))
+
+    print("With dict keys:")
+    print(remove_duplicates_dict(st))
+    print(remove_duplicates_dict(fruits))
