eff550457206a40bdea15b033cabdd32920bfee0
sriram <sriramgunda@yahoo.com>
2021-04-17 00:16:24 +0530

    generate random alpha numeric string

--- passwordgenerator/password_generator.py
+++ passwordgenerator/password_generator.py
@@ -7,6 +7,7 @@ dgts = string.digits
 special_chars = string.punctuation
 
 all_combo_keys = all_aplhabets + dgts + special_chars
+alpha_numeric_combo_keys = all_aplhabets + dgts
 
 def randomPassword(password_length = 10):
     '''
@@ -17,8 +18,18 @@ def randomPassword(password_length = 10):
     return ''.join(random_keys)
 
 
+def randomPassword_alpha_numeric(password_length = 10):
+    '''
+    Password contains combination of alpha numeric with default length as 10 chars.
+    '''
+    random_keys = [random.choice(alpha_numeric_combo_keys) for i in range(password_length)]
+    return ''.join(random_keys)
+
+
 if __name__ == '__main__':
     #default length of random password
     print(randomPassword())
     #8 characters length of password
     print(randomPassword(password_length = 8))
+    #generate alpha numeric hashkey
+    print(randomPassword_alpha_numeric(password_length = 40))
